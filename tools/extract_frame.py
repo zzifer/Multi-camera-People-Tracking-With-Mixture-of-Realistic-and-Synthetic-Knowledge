70b72b9942f25c1eabfc4c39c25dee83d3fda24f
Feng <52739675+zzifer@users.noreply.github.com>
2023-10-23 13:46:00 +0800

    Update extract_frame.py
    add notes

--- tools/extract_frame.py
+++ tools/extract_frame.py
@@ -10,7 +10,9 @@ from multiprocessing import Pool
 from sys import stdout
 import numpy as np
 
+# 加载配置文件，将配置信息存储在cfg变量中
 cfg = util.load_defaults()
+# 从配置文件中获取数据集的根目录路径
 ROOT_DATA_DIR = cfg["DATASETS"]["ROOT_DIR"]
 print(ROOT_DATA_DIR)
 
@@ -21,12 +23,16 @@ ROLES = ["train", "validation", "test"]
 IMAGE_FORMAT = ".jpg"  # ".png"
 
 
+# 将视频文件转换成图像序列
+# 函数接受一个包含四个元素的参数parameter_set，依次表示角色、场景、摄像机和摄像机目录
 def video2image(parameter_set):
     role, scenario, camera, camera_dir = parameter_set
     fprint(f"[Processing] {role} {scenario} {camera}{endl}")
+    # 定义了保存图像序列的目录，这个目录是根据摄像机目录构建的
     imgs_dir = f"{camera_dir}/img1"
     if not os.path.exists(imgs_dir):
         os.makedirs(imgs_dir)
+    # 通过cv2.VideoCapture打开视频文件，然后使用一个循环来逐帧读取视频帧，并将每一帧保存为图像文件
     cap = cv2.VideoCapture(f"{camera_dir}/video.mp4")
     current_frame = 1
     ret, frame = cap.read()
@@ -39,6 +45,7 @@ def video2image(parameter_set):
 
 
 def main():
+    # 使用嵌套循环遍历不同的角色、场景和摄像机，将参数集合添加到parameter_sets中
     parameter_sets = []
     for each_role in ROLES:
         role_dir = f"{ROOT_DATA_DIR}/{each_role}"
@@ -57,6 +64,7 @@ def main():
                     [each_role, each_scenario, each_camera, camera_dir]
                 )
 
+    # 使用multiprocessing.Pool来创建一个包含15个进程的进程池，然后使用pool.map来并行地调用video2image函数来处理参数集合
     pool = Pool(processes=15)
     pool.map(video2image, parameter_sets)
     pool.close()
